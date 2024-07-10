6368c7eb03734be79e8c415302346a978770380c
ms-hollow <leonen.abbegail@gmail.com>
2024-07-10 18:54:22 +0800

    in progress projects

--- src/components/homepage/Projects.jsx
+++ src/components/homepage/Projects.jsx
@@ -1,7 +1,8 @@
 import Projects from "../ui/ProjectUI";
 import Heading from "../ui/Heading";
+import image from "../../assets/icons/akkey.webp"
 
-export default function Works({ forwardedRef }) {
+export default function Projects({ forwardedRef }) {
   return (
     <section
       ref={forwardedRef}
@@ -12,19 +13,61 @@ export default function Works({ forwardedRef }) {
       <div className="mt-10 grid grid-cols-1 gap-16 gap-y-10 md:grid-cols-12">
         {/* Project #1 */}
         <div className=" col-span-1 md:col-span-12">
-          
+          <Projects
+            link="https://musical-stroopwafel-1c2327.netlify.app/landing.html"
+            img={image}
+            alt="IBGroup vietnam website mockup"
+            name="ibgroup vietnam website"
+            type="Web Design • Frontend Development"
+            year="2023"
+            tools="HTML • TailwindCSS • JavaScript • Figma"
+
+          />
         </div>
         {/* Project #2 */}
         <div className="col-span-1 pt-0 md:col-span-7 md:pt-16">
-          
+          <Projects
+            link="https://mementostudio.netlify.app/"
+            img={image}
+            alt="memento landing page mockup"
+            name="memento studio landing page"
+            type="Frontend Development"
+            year="2023"
+            tools="HTML • TailwindCSS • JavaScript"
+          />
         </div>
         <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
-          
+          <Projects
+            link="https://realbusinessaccountants.netlify.app"
+            img={image}
+            alt="real business accountant project mockup"
+            name="real business accountants"
+            type="Web Design • Frontend Development"
+            year="2023"
+            tools="HTML • CSS • JavaScript • Figma"
+          />
         </div>
         <div className="col-span-1 h-fit pt-0 md:col-span-8 md:pt-20">
-          
+          <Projects
+            link="https://godaddyuiclone.netlify.app"
+            img={image}
+            alt="godaddy clone page mockup"
+            name="GODADDY LANDING PAGE CLONE"
+            type="Frontend Development"
+            year="2023"
+            tools="HTML • TailwindCSS • JavaScript"
+          />
         </div>
-        <div className="col-span-1 h-fit md:col-span-4"> 
+        <div className="col-span-1 h-fit md:col-span-4">
+        <Projects
+            link="https://sunnysidechallenge.netlify.app"
+            img={image}
+            alt="sunnyside project mockup"
+            name="SUNNYSIDE LANDING PAGE"
+            type="Frontend Development"
+            year="2022"
+            tools="HTML • TailwindCSS • JavaScript"
+          />
         </div>
       </div>
     </section>
--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -69,6 +69,10 @@ export default function NavBar({ sectionRefs }) {
           <span>projects</span>
           <span className="absolute bottom-0 left-0 h-[0.125em] w-0 rounded-full bg-accent duration-300 ease-in-out group-hover:w-full"></span>
         </a>
+        <a href="https://dribbble.com/ms-akkey" className="group relative hidden md:inline-block">
+          <span>dribbble</span>
+          <span className="absolute bottom-0 left-0 h-[0.125em] w-0 rounded-full bg-accent duration-300 ease-in-out group-hover:w-full"></span>
+        </a>
         <a
           ref={cta}
           className="button group relative hover:bg-transparent"
@@ -76,7 +80,7 @@ export default function NavBar({ sectionRefs }) {
         >
           <span className="relative w-fit bg-accent px-4 py-2 rounded-full font-general font-medium text-body-3 hover:bg-secondary text-left">
           <span className="absolute bottom-2 left-0 h-[0.125em] w-0 rounded-full bg-primary duration-300 ease-out group-hover:w-full z-10"></span>
-            <span>Let&apos;s Talk.</span>
+            <span>Connect with me!</span>
           </span>
         </a>
       </nav>
